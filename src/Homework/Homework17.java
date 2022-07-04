813060798c021d1801e2ae2cb24f97a46bc0cb46
Olga <Kraisler60@ya.ru>
2022-07-04 12:32:56 -0400

    Initial commit " homework" update

--- src/Homework/Homework17.java
+++ src/Homework/Homework17.java
@@ -6,19 +6,23 @@ package Homework;
 public class Homework17 {
     public static void main(String[] args) {
 
-        float[] new_array = new float[]{5f, 67f, 3f, 76f, 54f, 12f, 78, 456f, 122f, 89f};
+        float[] new_array = new float[]{5f, 67f, 3f, 76f, 54f, 12f, 78, 456f, 122f, 22f};
         float a = new_array.length;
         for (int i = 0; i < a; i++) {
             System.out.println(new_array[i]);
         }
         //max
         float max = new_array[0];
-        for (int i = 0; i < a; i++) {
+       /* for (int i = 0; i < a; i++) {
             if (new_array[i] > max) ;{
-                max = new_array[i];
+                max = new_array[i];*/
+
+        for (float i: new_array) {
+            if (i>max);
+            max = i;
 
             }
-        }
+
             System.out.println(" max is " + max);
 
         }
